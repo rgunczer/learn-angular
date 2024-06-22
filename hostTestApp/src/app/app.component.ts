8c9b04a7ffa32ffbd0c143e7d31a01204fb3169e
Robert Gunczer <rgunczer@gmail.com>
2024-06-22 23:07:46 +0200

    css :host selector in component

---
+++ hostTestApp/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MyComponentComponent } from './my-component/my-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MyComponentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'hostTestApp';
+}
